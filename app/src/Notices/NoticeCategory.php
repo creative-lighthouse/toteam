9d1c98c68741e048b5ec6370d5143357e9d8280d
Steffen Kahl <stevenpaw209@gmail.com>
2025-11-06 23:33:55 +0100

    [~] Changed Version to 0.1.3

--- app/src/Notices/NoticeCategory.php
+++ app/src/Notices/NoticeCategory.php
@@ -8,8 +8,8 @@ use SilverStripe\ORM\DataObject;
  * Class \App\Notices\NoticeCategory
  *
  * @property ?string $Title
- * @mixin \SilverStripe\Assets\AssetControlExtension
  * @mixin \SilverStripe\Assets\Shortcodes\FileLinkTracking
+ * @mixin \SilverStripe\Assets\AssetControlExtension
  * @mixin \SilverStripe\CMS\Model\SiteTreeLinkTracking
  * @mixin \SilverStripe\Versioned\RecursivePublishable
  * @mixin \SilverStripe\Versioned\VersionedStateExtension
